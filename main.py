15318e945b136cb8adec76aa513e4299ba9f3759
Swayam Chidrawar <schidraw@calpoly.edu>
2023-02-05 22:02:20 -0800

    set .env variable for api key

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import os
 import streamlit as st
 import pandas as pd
 import numpy as np
 import openai
 import pyperclip
 
-openai.api_key = "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"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 model_engine = "text-davinci-003"
 
 st.title('Fake JSON Generator')
 
+
+st.header('Enter your JSON here')
 text_input = st.text_area('Enter your JSON here', height=500)
 
 prompt = "Generate sample JSON object list for the following interface. Please give me a valid json that is validated" + text_input
@@ -19,6 +22,7 @@ max_tokens = 350
 # Generate a response
 # Dont run completion if text_input is empty
 x = 0
+st.header('Generated JSON')
 if text_input and x != 1:
     completion = openai.Completion.create(
         engine=model_engine,
