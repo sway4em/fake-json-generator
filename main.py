4c7380c29acb18c17ab5ea911997481bc4c26a68
Swayam Chidrawar <schidraw@calpoly.edu>
2023-02-05 21:35:03 -0800

    add openai import and api key

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import openai
+import pyperclip
+
+openai.api_key = "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"
+model_engine = "text-davinci-003"
 
 st.title('Fake JSON Generator')
